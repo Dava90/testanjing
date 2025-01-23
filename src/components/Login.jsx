bb7981016a1813872dafc5b31a974e908e54504b
Dava90 <123545278+Dava90@users.noreply.github.com>
2025-01-23 07:48:20 +0700

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -9,7 +9,7 @@ const Login = ({ setToken }) => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("https://apitolol.vercel.app/api/login", {
+      const response = await fetch("https://apitolol2.vercel.app/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
